9da040df52dbb2b5e01c85927bde9394c25fe27d
thiagoramoscp <thiagoramoscp@gmail.com>
2021-06-17 17:33:16 -0300

    8kyu

---
+++ js/8kyu/a-bugs-trilogy-episode-1-let-math.random-decide-your-future.js
@@ -0,0 +1,14 @@
+/*
+correct sintax errors basically
+*/
+function yourFutureCareer() {
+	var career = Math.random();
+		if (career <= 0.32) {
+			return "FrontEnd Developer"
+		} else if (career <= 0.65) {
+			return "BackEnd Developer"
+		} else {
+			return 'Full-Stack Developer'
+		}
+};
+yourFutureCareer();
