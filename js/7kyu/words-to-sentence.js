9219c5f1ae6543c5be30140daee6429b61720924
thiagoramoscp <thiagoramoscp@gmail.com>
2021-08-26 16:06:19 -0300

    7kyu but not really

---
+++ js/7kyu/words-to-sentence.js
@@ -0,0 +1,11 @@
+/*
+Write function which will create a string from a list of strings, separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
+*/
+
+function wordsToSentence(words) {
+  return words.join(" ");
+}
