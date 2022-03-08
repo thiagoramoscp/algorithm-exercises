9715122deb83e30719caa833f63a4bb978a39c03
thiagoramoscp <thiagoramoscp@gmail.com>
2022-03-08 15:36:03 -0300

    6kyu

---
+++ js/6kyu/detect-pangram.js
@@ -0,0 +1,15 @@
+/*
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+*/
+
+function isPangram(string){
+  string = string.toLowerCase();
+  for (let i = 97; i <= 122; i++) {
+    if (!string.includes(String.fromCharCode(i))) {
+      return false;
+    } 
+  }
+  return true;
+}
