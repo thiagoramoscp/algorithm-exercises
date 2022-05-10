4497b1d392d8fb936407a7dd2059786fa91d30a9
thiagoramoscp <thiagoramoscp@gmail.com>
2022-05-10 00:38:39 -0300

    8kyu

---
+++ java/8kyu/convert-a-boolean-to-a-string.java
@@ -0,0 +1,23 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+public class BooleanToString {
+  public static String convert(boolean b){
+    return String.valueOf(b);
+  }
+
+}
+    /*
+    if(b == true) {
+      return "true";
+    } else {
+      return "false";
+    }
+
+    b ? "true" : "false";
+
+    Boolean.toString(b);
+    */
